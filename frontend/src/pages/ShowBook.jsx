66779ac805939ae98051f106c955fadc7e8df364
Nishan Bhandari <bnishan18@gmail.com>
2024-04-27 22:05:30 +0545

    Frontend Completed

--- frontend/src/pages/ShowBook.jsx
+++ frontend/src/pages/ShowBook.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { useAsyncError, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import BackButton from "../components/BackButton";
 import Spinner from "../components/Spinner";
 
@@ -12,7 +12,7 @@ const ShowBook = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get(`http://localhost:5555/${id}`)
+      .get(`http://localhost:5555/books/${id}`)
       .then((response) => {
         setBook(response.data);
         setLoading(false);
@@ -24,44 +24,40 @@ const ShowBook = () => {
   }, []);
 
   return (
-    <>
-      <div className="p-4">
-        <BackButton />
-        <h1 className="text-3xl my-4">Show Book</h1>
-        {loading ? (
-          <Spinner />
-        ) : (
-          <div className="flex flex-col border-2 border-sky-400 rounded-xl w-fit p-4">
-            <div className="my-4">
-              <span className="text-xl mr-4 text-gray-500">Id</span>
-              <span>{book._id}</span>
-            </div>
-            <div className="my-4">
-              <span className="text-xl mr-4 text-gray-500">Title</span>
-              <span>{book.title}</span>
-            </div>
-            <div className="my-4">
-              <span className="text-xl mr-4 text-gray-500">Author</span>
-              <span>{book.author}</span>
-            </div>
-            <div className="my-4">
-              <span className="text-xl mr-4 text-gray-500">publishYear</span>
-              <span>{book.publishYear}</span>
-            </div>
-            <div className="my-4">
-              <span className="text-xl mr-4 text-gray-500">Create Time</span>
-              <span>{new Date(book.createdAt).toString()}</span>
-            </div>
-            <div className="my-4">
-              <span className="text-xl mr-4 text-gray-500">
-                Last Update Time
-              </span>
-              <span>{new Date(book.createdAt).toString()}</span>
-            </div>
+    <div className="p-4">
+      <BackButton />
+      <h1 className="text-3xl my-4">Show Book</h1>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <div className="flex flex-col border-2 border-sky-400 rounded-xl w-fit p-4">
+          <div className="my-4">
+            <span className="text-xl mr-4 text-gray-500">Id</span>
+            <span>{book._id}</span>
           </div>
-        )}
-      </div>
-    </>
+          <div className="my-4">
+            <span className="text-xl mr-4 text-gray-500">Title</span>
+            <span>{book.title}</span>
+          </div>
+          <div className="my-4">
+            <span className="text-xl mr-4 text-gray-500">Author</span>
+            <span>{book.author}</span>
+          </div>
+          <div className="my-4">
+            <span className="text-xl mr-4 text-gray-500">Publish Year</span>
+            <span>{book.publishYear}</span>
+          </div>
+          <div className="my-4">
+            <span className="text-xl mr-4 text-gray-500">Create Time</span>
+            <span>{new Date(book.createdAt).toString()}</span>
+          </div>
+          <div className="my-4">
+            <span className="text-xl mr-4 text-gray-500">Last Update Time</span>
+            <span>{new Date(book.updatedAt).toString()}</span>
+          </div>
+        </div>
+      )}
+    </div>
   );
 };
 
